e5ca0719b41d7b5496f72df7c1e294d62f8738f0
Cooper Hendrix <cwhendrix3@icloud.com>
2022-07-18 15:50:25 -0400

    Added navigation bar that mostly works right

--- Summer Research Project 2022/ContentView.swift
+++ Summer Research Project 2022/ContentView.swift
@@ -8,62 +8,36 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     
     var body: some View {
         
-            NavigationView{
-                ZStack{
-                    VStack {
-                        Text("RGB Engineers")
-                            .fontWeight(.bold).padding(.bottom).dynamicTypeSize(/*@START_MENU_TOKEN@*/.accessibility2/*@END_MENU_TOKEN@*/)
-                        
-                        Spacer()
-                        
-                        NavigationLink {
-                            CameraView()
-                        } label: {
-                            Image("Analyze a Sample")
-                                .aspectRatio(contentMode: .fit)
-                                .padding()
-                                .frame(width: 290.0, height: 120.0)
-                                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
-                        }
-                        
-                        Spacer()
-                    
-                        NavigationLink{
-                            PastTestsView()
-                        }
-                    label:{
-                        Image("View Past Tests")
-                            .aspectRatio(contentMode: .fit)
-                            .padding()
-                            .frame(width: 290.0, height: 120.0)
-                            .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
-                        }
-                        
-                        Spacer()
-                        
-                        NavigationLink{
-                            Manual()
-                        }
-                    label:{
-                        Image("Instructions")
-                            .aspectRatio(contentMode: .fit)
-                            .padding()
-                            .frame(width: 175.0, height: 60.0)
-                            .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
-                            
-                        }
-                        
-                        Spacer()
-                            
-
-                    }
+        TabView{
+            HomePage()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                    //broken atm
                 }
-            }.navigationBarBackButtonHidden(true).navigationBarHidden(true)
+            CameraView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Analyze")
+                }
+            PastTestsView()
+                .tabItem {
+                    Image(systemName: "clock")
+                    Text("Past Tests")
+                }
+            Manual()
+                .tabItem {
+                    Image(systemName: "book")
+                    Text("Instructions")
+                }
             
+        }
+        
     }
 }
 

a48b3fc281083cdd9965026ba97b789cf5982429
Cooper Hendrix <cwhendrix3@icloud.com>
2022-07-29 13:26:49 -0400

    UI

--- Summer Research Project 2022/CroppedView.swift
+++ Summer Research Project 2022/CroppedView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct CroppedView: View {
-    var testResults: results!
+    var testResults: results = results()
+    
     var body: some View {
         let screenSize: CGRect = UIScreen.main.bounds
         let screenHeight = screenSize.height
@@ -22,13 +23,50 @@ struct CroppedView: View {
                 .frame(width: screenWidth * 7/8, height: (screenHeight * 4/5))
                 .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
             VStack{
+                Text("RGB Values")
+                    .fontWeight(.bold)
+                    .padding(.bottom)
+                    .dynamicTypeSize(/*@START_MENU_TOKEN@*/.accessibility2/*@END_MENU_TOKEN@*/)
                 Spacer()
-                Text("Top Left: \(testResults.test1Left)")
-                Text("Top Right: \(testResults.test1Right)")
-                Text("Middle Left: \(testResults.test2Left)")
-                Text("Middle Right: \(testResults.test2Right)")
-                Text("Bottom Left: \(testResults.test3Left)")
-                Text("Bottom Right: \(testResults.test3Right)")
+                HStack{
+                    Text("Top Left: \(testResults.test1Left)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.all)
+                        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.913))
+                        .cornerRadius(20)
+                    Text("Top Right: \(testResults.test1Right)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.all)
+                        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.913))
+                        .cornerRadius(20)
+                }
+                
+                HStack{
+                    Text("Middle Left: \(testResults.test2Left)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.all)
+                        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.913))
+                        .cornerRadius(20)
+                    Text("Middle Right: \(testResults.test2Right)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.all)
+                        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.913))
+                        .cornerRadius(20)
+                }
+                
+                HStack{
+                    Text("Bottom Left: \(testResults.test3Left)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.all)
+                        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.913))
+                        .cornerRadius(20)
+                    Text("Bottom Right: \(testResults.test3Right)")
+                        .multilineTextAlignment(.leading)
+                        .padding(.all)
+                        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.913))
+                        .cornerRadius(20)
+                }
+                
                 Spacer()
                 NavigationLink{
                     ContentView()
